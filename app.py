b958a284d131577a1211ee098450dee64f3720d5
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2023-04-12 12:37:47 +0000

    Complete Task - setup file structur - Used sqlite to sort out database - crud operations for user
    Next tasks
    - Password hashing and authentication
    - Token generation
    - Google map api integration

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+
+from api.database import engine, Base
+from api.routes import user_routes
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.include_router(user_routes.router)
 
 @app.get('/')
 async def home():
     return {"Hello World"}
+
