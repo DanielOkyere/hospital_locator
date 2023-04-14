c096b9d7bdab247aa1a0d68eb8f6b013b793bde4
tobexint <tobexint@gmail.com>
2023-04-14 11:05:51 +0000

    app.py test

---
+++ tests/test_app.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+from app.py import app
+
+client = TestClient(app)
+
+def test_home():
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
