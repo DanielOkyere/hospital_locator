847469db0109b18ac2bb6001ca4871f7ce621b83
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2023-04-21 09:15:33 +0000

    Updates - added routes for fetching hospitals - added routes for fetching hospital based on name - added routes to fetch hospital based on id

--- api/models/hospital/hospitals.py
+++ api/models/hospital/hospitals.py
@@ -1,14 +1,16 @@
 from api.database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Float
 from geoalchemy2 import Geometry
 
 
 class Hospital(Base):
     """Model for Class"""
-    
+
     __tablename__ = 'hospital'
     id = Column(Integer, nullable=False, primary_key=True)
     name = Column(String, nullable=False, unique=True)
-    coordinates=Column(Geometry(geometry_type='POINT', srid=4326))
-    specialisation=Column(String)
-    address=Column(String)
+    coordinates = Column(Geometry(geometry_type='POINT', srid=4326))
+    longitude = Column(Float)
+    latitude = Column(Float)
+    specialisation = Column(String)
+    address = Column(String)
--- api/models/hospital/schemas.py
+++ api/models/hospital/schemas.py
@@ -1,20 +1,16 @@
 from pydantic import BaseModel
 
+
 class HospitalBase(BaseModel):
     id: int
+
 
 class HospitalCreate(HospitalBase):
     name: str
     specialisation: str
     address: str
+    longitude: float
+    latitude: float
 
-    
     class Config:
         orm_mode = True
-        
-        
-class HospitalOutputSchema(HospitalBase):
-    name: str
-    coordinates: dict
-    specialisation: str
-    address: str
