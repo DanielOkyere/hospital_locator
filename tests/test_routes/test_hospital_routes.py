5236753926cb55c70c218bd7cb58b7c6177df8d3
Tobex <89360416+Tobexint@users.noreply.github.com>
2023-04-26 14:56:10 +0000

    Update test_hospital_routes.py

--- tests/test_routes/test_hospital_routes.py
+++ tests/test_routes/test_hospital_routes.py
@@ -9,3 +9,23 @@ def test_get_hospitals(client):
        assert response.json(){"detail":"Hospital not found"}
     assert response.json()["skip"] == "skip"
     assert response.json()["limit"] == "limit"
+
+def test_get_hospitals_by_name(client):
+    data = {"response_model":"list[schemas.HospitalCreate]", "hospital_name": "str", "skip":"int = 0", "limit":"int = 20"}
+    response = client.get("/{hospital_name}", json.dumps(data))
+    assert response.status_code == 200
+    if hospitals is None:
+       assert response.status_code == 404
+       assert response.json(){"detail":"Hospital not found"}
+    assert response.json()["hospital_name"] == "hospital_name"
+    assert response.json()["skip"] == "skip"
+    assert response.json()["limit"] == "limit"
+
+def test_get_hospitals_by_id(client):
+    data = {"response_model":"schemas.HospitalCreate", "hospital_id":"int"}
+    response = client.get("/id/{hospital_id}", json.dumps(data)
+    assert response.status_code == 200
+    if hospitals is None:
+       assert response.status_code == 404
+       assert response.json(){"detail":"Hospital not found"}
+    assert response.json()["hospital_id"] == "hospital_id"
