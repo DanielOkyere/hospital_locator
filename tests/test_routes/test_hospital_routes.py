b8ffd71d19db123ec6f6f1353b6880727e39462f
Tobex <89360416+Tobexint@users.noreply.github.com>
2023-04-26 14:18:05 +0000

    Update test_hospital_routes.py

--- tests/test_routes/test_hospital_routes.py
+++ tests/test_routes/test_hospital_routes.py
@@ -1,1 +1,11 @@
 import json
+
+def test_get_hospitals(client):
+    data = {"response_model": "list[schemas.HospitalCreate]", "skip":"int = 0", "limit": "int = 100"}
+    response= client.get("/", json.dumps(data))
+    assert response.status_code == 200
+    if hospitals is None:
+       assert response.status_code == 404
+       assert response.json(){"detail":"Hospital not found"}
+    assert response.json()["skip"] == "skip"
+    assert response.json()["limit"] == "limit"
